4954f3f5938a5fcc59a8ee55748e191e1dce9564
uct-trk <72524087+uct-trk@users.noreply.github.com>
2021-03-27 16:26:27 +0300

    UCT GOD

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/*CUSTOM ALL - CAROUSEL */
+ $(document).ready(function () {
+
+    let owl = $(".owl-carousel").owlCarousel({
+        autoplay: false,
+        dots: true,
+        dotsData : true,
+        loop : true,
+        nav : false,
+        items: 1
+    });
+
+    $('.owl-dot').click(function(){
+        owl.trigger('to.owl.carousel',[$(this).index(), 2000])
+    })
+});
+
+
+/*OPEN-CLOSE MENU */
+
+$('#chek').change(function(){
+    if($(this).is(":checked")) {
+        $('.nav-header').addClass('active');    
+    } else {
+        $('.nav-header').removeClass('active');
+    }
+});
+
+
+
